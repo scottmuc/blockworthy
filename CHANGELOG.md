043ce4ab6021199e6a54ea6149775196686e5373
Scott Muc <scottmuc@gmail.com>
2014-08-29 14:42:44 +0300

    can block a list of domains

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,11 @@
 # Changelog
 All notable changes to this project will be documented in this file.
 
+## 0.0.2 - 2014-08-29
+
+### Added
+- Can block a list of domains now (buzzfeed, viralnova, upworthy)
+
 ## 0.0.1 - 2014-08-29
 
 ### Added
--- contentScript.js
+++ contentScript.js
@@ -1,13 +1,34 @@
 
-var links = document.links;
+function domain_is_blockworthy(domain) {
+  var blockworthy_domains = [
+    "buzzfeed.com",
+    "viralnova.com",
+    "upworthy.com"
+  ];
 
+  for(var i = 0; i < blockworthy_domains.length; i++) {
+    blockworthy_domain = blockworthy_domains[i];
+    if (domain == blockworthy_domain || domain == "www." + blockworthy_domain) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+function scrub(link) {
+  link.href = "https://en.wikipedia.org/wiki/Clickbait";
+  link.textContent = "SCRUBBED BY BLOCKWORTHY";
+}
+
+
+var links = document.links;
 var size = links.length
+
 for(var i = 0; i < size; i++) {
   var link = links[i];
-
-  if (link.hostname == "buzzfeed.com" || link.hostname == "www.buzzfeed.com") {
-    link.href = "https://en.wikipedia.org/wiki/Clickbait";
-    link.textContent = "SCRUBBED BY BLOCKWORTHY";
+  if (domain_is_blockworthy(link.hostname)) {
+    scrub(link);
   }
 }
 
