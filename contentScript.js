200a13b20232d299e4fce402805cb391ef06e0a0
Scott Muc <scottmuc@gmail.com>
2014-08-30 13:25:01 +0300

    get blockworthy domains from local storage

--- contentScript.js
+++ contentScript.js
@@ -1,18 +1,16 @@
+var default_blockworthy_domains = [
+  "buzzfeed.com",
+  "viralnova.com",
+  "upworthy.com"
+];
 
-function domain_is_blockworthy(domain) {
-  var blockworthy_domains = [
-    "buzzfeed.com",
-    "viralnova.com",
-    "upworthy.com"
-  ];
-
+function domain_is_blockworthy(blockworthy_domains, domain) {
   for(var i = 0; i < blockworthy_domains.length; i++) {
     blockworthy_domain = blockworthy_domains[i];
     if (domain == blockworthy_domain || domain == "www." + blockworthy_domain) {
       return true;
     }
   }
-
   return false;
 }
 
@@ -21,14 +19,17 @@ function scrub(link) {
   link.textContent = "SCRUBBED BY BLOCKWORTHY";
 }
 
+chrome.storage.local.get({
+  'blockworthy_domains': default_blockworthy_domains
+}, function(options) {
+  var links = document.links;
+  var size = links.length
 
-var links = document.links;
-var size = links.length
-
-for(var i = 0; i < size; i++) {
-  var link = links[i];
-  if (domain_is_blockworthy(link.hostname)) {
-    scrub(link);
+  for(var i = 0; i < size; i++) {
+    var link = links[i];
+    if (domain_is_blockworthy(options.blockworthy_domains, link.hostname)) {
+      scrub(link);
+    }
   }
-}
+});
 
